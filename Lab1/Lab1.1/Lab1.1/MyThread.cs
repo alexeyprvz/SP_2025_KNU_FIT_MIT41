00b8b851cd24fc14b1d617bba05134dc37d400d1
alexeyprvz <alexeyprvz@knu.ua>
2025-04-01 03:52:12 +0300

    lab1

---
+++ Lab1/Lab1.1/Lab1.1/MyThread.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._2
+{
+    class MyThread
+    {
+        public long Count;
+        public Thread Thrd;
+        static bool stop = false;
+        static string currentName;
+
+        public MyThread(string name)
+        {
+            Count = 0;
+            Thrd = new Thread(Run);
+            Thrd.Name = name;
+            currentName = name;
+        }
+
+        void Run()
+        {
+            Console.WriteLine("Thread " + Thrd.Name + " is beginning.");
+            do
+            {
+                Count++;
+                if (currentName != Thrd.Name)
+                {
+                    currentName = Thrd.Name;
+                    Console.WriteLine("In thread " + currentName);
+                }
+            } while (!stop && Count < 1e9);
+            stop = true;
+            Console.WriteLine("Thread " + Thrd.Name + " is completed.");
+        }
+    }
+}
+
---
+++ Lab1/Lab1.1/Lab1.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using Lab1._2;
+
+class Program
+{
+    static void Main()
+    {
+        MyThread mt1 = new MyThread("Thread 1 (Normal/Середній)");
+        MyThread mt2 = new MyThread("Thread 2 (AboveNormal/Вище середнього)");
+        MyThread mt3 = new MyThread("Thread 3 (Highest/Найвищий)");
+
+        mt1.Thrd.Priority = ThreadPriority.Normal;
+        mt2.Thrd.Priority = ThreadPriority.AboveNormal;
+        mt3.Thrd.Priority = ThreadPriority.Highest;
+
+        mt1.Thrd.Start();
+        mt2.Thrd.Start();
+        mt3.Thrd.Start();
+
+        mt1.Thrd.Join();
+        mt2.Thrd.Join();
+        mt3.Thrd.Join();
+
+        long total = mt1.Count + mt2.Count + mt3.Count; // Загальна к-ть Count
+
+        double p1 = (double)mt1.Count / total * 100; // Обчислення для кожного у відсотках
+        double p2 = (double)mt2.Count / total * 100;
+        double p3 = (double)mt3.Count / total * 100;
+
+        Console.WriteLine("\n=== Результати ===");
+        Console.WriteLine($"Thread {mt1.Thrd.Name}: {mt1.Count} iterations (~{p1:F2}%)");
+        Console.WriteLine($"Thread {mt2.Thrd.Name}: {mt2.Count} iterations (~{p2:F2}%)");
+        Console.WriteLine($"Thread {mt3.Thrd.Name}: {mt3.Count} iterations (~{p3:F2}%)");
+
+        Console.ReadLine();
+    }
+}
